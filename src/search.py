7524369cf269929f11408355ca4b1934a8b23e4e
Timothy Reynolds <reynolds@lazlo.ecs.baylor.edu>
2015-08-25 14:56:18 -0500

    Changed sphinx search options

--- src/search.py
+++ src/search.py
@@ -3,7 +3,7 @@ import geneweaverdb
 import sphinxapi
 
 #Sphinx server connection information
-sphinx_server = 'localhost'
+sphinx_server = 'bepo.ecs.baylor.edu'
 #sphinx_server = 'localhost'
 sphinx_port = 9312
 #The number of maximum search results to return (not by page, but in total)
