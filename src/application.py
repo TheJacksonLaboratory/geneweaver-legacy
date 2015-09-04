d59f91f454a4d8011d46187dcd30b84b12da8a8f
kelechi_fletcher <kelechi_fletcher@baylor.edu>
2015-09-04 13:55:43 -0500

    Reconfigured

--- src/application.py
+++ src/application.py
@@ -1,3 +1,4 @@
+#Kelechi was here
 import flask
 from flask.ext.admin import Admin, BaseView, expose
 from flask.ext.admin.base import MenuLink
@@ -85,7 +86,7 @@ admin.add_link(MenuLink(name='My Account', url='/accountsettings.html'))
 
 #*************************************
 
-RESULTS_PATH = '/var/www/html/dev-geneweaver/results'
+RESULTS_PATH = '/Users/group15admin/geneweaver/results'
 HOMOLOGY_BOX_COLORS = ['#58D87E', '#588C7E', '#F2E394', '#1F77B4', '#F2AE72', '#F2AF28', 'empty', '#D96459',
                        '#D93459', '#5E228B', '#698FC6']
 SPECIES_NAMES = ['Mus musculus', 'Homo sapiens', 'Rattus norvegicus', 'Danio rerio', 'Drosophila melanogaster',
