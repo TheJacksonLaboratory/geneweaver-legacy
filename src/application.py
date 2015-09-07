39ed5542988c53ffff1f63760ef8de43dfc11b75
group16admin <kevin_wang@baylor.edu>
2015-09-07 16:37:55 -0500

    Quick comment

--- src/application.py
+++ src/application.py
@@ -40,7 +40,7 @@ app.secret_key = '\x91\xe6\x1e \xb2\xc0\xb7\x0e\xd4f\x058q\xad\xb0V\xe1\xf22\xa5
 
 
 #*************************************
-
+# Comment
 admin = Admin(app, name='Geneweaver', index_view=adminviews.AdminHome(
     url='/admin', name='Admin'))
 
