cd34fa968021227576a3d491d1b61cf78f4c098a
Melissa_Curtis <Melissa_Curtis@baylor.edu>
2015-10-05 10:47:20 -0500

    Test logic

--- src/tools/tricliqueblueprint.py
+++ src/tools/tricliqueblueprint.py
@@ -52,19 +52,18 @@ def run_tool():
             if params[tool_param.name] != 'Enabled':
                 params[tool_param.name] = 'Disabled'
                 m = 1
-        if not n and not m:
-            flask.flash("You must enable either Exact Gene Overlap or Jaccard")
-            return flask.redirect('analyze')
-        elif n and m:
-            flask.flash("You can run either Jaccard or Exact Gene Overlap. Select one.")
-            return flask.redirect('analyze')
-        elif n and not m and len(selected_project_ids) < 3:
+        if n and not m and len(selected_project_ids) < 3:
             flask.flash("Warning: You need at least 3 projects!")
             return flask.redirect('analyze')
         elif m and not n and len(selected_project_ids) != 2:
             flask.flash("Warning: You must select 2 projects!")
             return flask.redirect('analyze')
-
+        elif not n and not m:
+            flask.flash("You must enable either Exact Gene Overlap or Jaccard")
+            return flask.redirect('analyze')
+        elif n and m:
+            flask.flash("You can run either Jaccard or Exact Gene Overlap. Select one.")
+            return flask.redirect('analyze')
 
     # TODO include logic for "use emphasis" (see prepareRun2(...) in Analyze.php)
 
