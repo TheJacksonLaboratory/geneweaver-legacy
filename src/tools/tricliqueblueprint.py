c5525b8d3d0a9540545dac90caee7cef52d401e3
Melissa_Curtis <Melissa_Curtis@baylor.edu>
2015-10-05 10:42:17 -0500

    Test logic

--- src/tools/tricliqueblueprint.py
+++ src/tools/tricliqueblueprint.py
@@ -43,27 +43,27 @@ def run_tool():
 
     n = 0
     m = 0
-    #for tool_param in gwdb.get_tool_params(TOOL_CLASSNAME, True):
-    if tool_param.name.endswith('_ExactGeneOverlap'):
-        if params[tool_param.name] != 'Enabled':
-            params[tool_param.name] = 'Disabled'
-            n = 1
-    if tool_param.name.endswith('_Jaccard'):
-        if params[tool_param.name] != 'Enabled':
-            params[tool_param.name] = 'Disabled'
-            m = 1
-    if n and m:
-        flask.flash("You must enable either Exact Gene Overlap or Jaccard")
-        return flask.redirect('analyze')
-    elif not n and not m:
-        flask.flash("You can run either Jaccard or Exact Gene Overlap. Select one.")
-        return flask.redirect('analyze')
-    elif n and not m and len(selected_project_ids) < 3:
-        flask.flash("Warning: You need at least 3 projects!")
-        return flask.redirect('analyze')
-    elif m and not n and len(selected_project_ids) != 2:
-        flask.flash("Warning: You must select 2 projects!")
-        return flask.redirect('analyze')
+    for tool_param in gwdb.get_tool_params(TOOL_CLASSNAME, True):
+        if tool_param.name.endswith('_ExactGeneOverlap'):
+            if params[tool_param.name] != 'Enabled':
+                params[tool_param.name] = 'Disabled'
+                n = 1
+        if tool_param.name.endswith('_Jaccard'):
+            if params[tool_param.name] != 'Enabled':
+                params[tool_param.name] = 'Disabled'
+                m = 1
+        if not n and not m:
+            flask.flash("You must enable either Exact Gene Overlap or Jaccard")
+            return flask.redirect('analyze')
+        elif n and m:
+            flask.flash("You can run either Jaccard or Exact Gene Overlap. Select one.")
+            return flask.redirect('analyze')
+        elif n and not m and len(selected_project_ids) < 3:
+            flask.flash("Warning: You need at least 3 projects!")
+            return flask.redirect('analyze')
+        elif m and not n and len(selected_project_ids) != 2:
+            flask.flash("Warning: You must select 2 projects!")
+            return flask.redirect('analyze')
 
 
     # TODO include logic for "use emphasis" (see prepareRun2(...) in Analyze.php)
