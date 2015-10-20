099136076a44f180489390b0655bb42bdc297870
drbodden <David_Bodden@baylor.edu>
2015-10-20 21:36:59 +0000

    application.py edited online with Bitbucket

--- src/application.py
+++ src/application.py
@@ -91,7 +91,7 @@ admin.add_link(MenuLink(name='My Account', url='/accountsettings.html'))
 #*************************************
 
 # changed this path 9/3
-RESULTS_PATH = '/Users/group15admin/geneweaver/results'
+RESULTS_PATH = '/var/www/html/geneweaver/results'
 
 HOMOLOGY_BOX_COLORS = ['#58D87E', '#588C7E', '#F2E394', '#1F77B4', '#F2AE72', '#F2AF28', 'empty', '#D96459',
                        '#D93459', '#5E228B', '#698FC6']
