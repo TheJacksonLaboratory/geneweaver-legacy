61753e86415e2ce7025f501cf0060f562e0a82f7
Melissa_Curtis <Melissa_Curtis@baylor.edu>
2015-11-14 14:53:14 -0600

    Accessing geneset names based on ode gene ids

--- src/geneweaverdb.py
+++ src/geneweaverdb.py
@@ -14,7 +14,7 @@ from flask import session
 
 app = flask.Flask(__name__)
 
-RESULTS_PATH = '/Users/group6admin/Documents/geneweaver/results/'
+RESULTS_PATH = '/Users/group6admin/geneweaver/results/'
 
 
 
