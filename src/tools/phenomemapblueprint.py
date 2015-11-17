f491ce9a7b4bd061bc37162e341c345e5de19eac
group1admin <benjamin_cody@baylor.edu>
2015-11-17 16:03:20 -0600

    Got rid of collision detection; going to just go with charge repulsion for now and see how things go

--- src/tools/phenomemapblueprint.py
+++ src/tools/phenomemapblueprint.py
@@ -203,7 +203,7 @@ def view_result(task_id):
         # TODO render a real descriptive error page not just an exception
         raise Exception('error while processing: ' + tool.name)
     elif async_result.state in states.READY_STATES:
-        f = open('/Users/group4admin/PycharmProjects/results/TestOutput.hisim.json', 'r');
+        f = open('../../../results/TestOutput.hisim.json', 'r');
         data = ''
         for line in f:
             data += str(line)
