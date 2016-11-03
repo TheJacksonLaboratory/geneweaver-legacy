9bcaba8ce7fb66fc6ba8d56a754a870939aab97c
Austin Anderson <Austin.David.Anderson1@gmail.com>
2016-11-03 09:26:05 -0500

    commented everything so you can read it for presentation

--- src/tools/mset/mset.cpp
+++ src/tools/mset/mset.cpp
@@ -2,11 +2,11 @@
 
 int main(int argc, char** argv){
     if(argc<6){
-        cerr<<"expected <num samples> <topFile path> <background filepath> <number of interestFiles> <set-of-intrest filepath>"<<endl; //TODO: change to log file, add support for multiple files
+        cerr<<"expected <num samples> <topFile path> <background filepath> <number of interestFiles> <set-of-intrest filepath>"<<endl; //TODO: change to log file
         exit(1);
     }
     //*    //string
-    MSET<string> msetFinder;
+    MSET<string> msetFinder;//main logic is in this mset class, which is templated incase requirements change
     /*/   //numeric
     MSET<int> msetFinder;
     //*/
@@ -14,8 +14,8 @@ int main(int argc, char** argv){
     string topFile=argv[2];
     string backgroundFile=argv[3];
     int numInterestFiles=atoi(argv[4]);
-    cout<<"{\"data\":["<<endl;
-    cout<<msetFinder.run(numSamples,topFile,backgroundFile,argv[5]);
+    cout<<"{\"data\":["<<endl;//the cpp just puts the json objects into an array
+    cout<<msetFinder.run(numSamples,topFile,backgroundFile,argv[5]);//run function outputs a json object of the results of the mset run
     for(int i=1;i<numInterestFiles;i++){
         cout<<","<<endl<<msetFinder.run(numSamples,topFile,backgroundFile,argv[i+5]);
     }
