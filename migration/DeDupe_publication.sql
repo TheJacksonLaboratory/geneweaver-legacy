c745cb1606adaf8470d0130074fb5d0493542280
Alexander Berger <aberger@jax.org>
2016-11-15 21:28:53 +0000

    Added sanity-check to see if query was successful, and added comments to identify code sections.

--- migration/DeDupe_publication.sql
+++ migration/DeDupe_publication.sql
@@ -1,7 +1,9 @@
 
+/* Disable geneset_gs_updated Trigger */
 DROP TRIGGER IF EXISTS geneset_gs_updated
 ON production.geneset;
 
+/* Update Foreign Keys */
 WITH plan AS 
 (
   SELECT pub_id, pub_pubmed, min(pub_id) OVER (PARTITION BY pub_pubmed) AS master_pub_id
@@ -15,13 +17,18 @@ upd_geneset AS
   WHERE  g.pub_id = p.pub_id
   AND    p.pub_id <> p.master_pub_id
 )
-   
+
+/* Delete Duplicates */   
 DELETE FROM production.publication c
 USING  plan p
 WHERE  c.pub_id = p.pub_id
 AND    p.pub_id <> p.master_pub_id
 RETURNING c.pub_id;
 
+/* Re-Enable geneset_gs_updated Trigger */
 CREATE TRIGGER geneset_gs_updated
 BEFORE UPDATE ON production.geneset
 FOR EACH ROW EXECUTE PROCEDURE production.on_geneset_updated();
+
+/* Sanity-check: Should return 0 rows upon success */
+SELECT pub_id, pub_pubmed, count(pub_pubmed) FROM production.publication GROUP BY pub_id, pub_pubmed HAVING count(pub_pubmed) > 1;
