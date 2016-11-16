8b9e85c3177b2372e6d842428866495f27f8ea8b
Alexander Berger <aberger@jax.org>
2016-11-16 14:30:06 +0000

    Change 'IN' to '=ANY(' in hopes of improving speed. Cleaned up formatting to be more consistent with syntax.

--- migration/Remove_orphan_users.sql
+++ migration/Remove_orphan_users.sql
@@ -1,19 +1,19 @@
 
 /* This Code is intended to delete usrs that are not referenced in any foreign key */
-DELETE FROM production.usr z WHERE z.usr_id IN (SELECT u.usr_id
-FROM production.usr u
-LEFT JOIN production.geneset g
-  ON g.usr_id = u.usr_id
-LEFT JOIN production.project p
-  ON p.usr_id = u.usr_id
-LEFT JOIN production.result r
-  ON r.usr_id = u.usr_id
-LEFT JOIN extsrc_20150605.usr2gene x
-  ON x.usr_id = u.usr_id
-LEFT JOIN production.usr2grp s
-  ON s.usr_id = u.usr_id
-WHERE g.usr_id is NULL
-AND p.usr_id is NULL
-AND r.usr_id is NULL
-AND x.usr_id is NULL
-AND s.usr_id is NULL);
+DELETE FROM production.usr z WHERE z.usr_id=ANY(
+SELECT u.usr_id FROM production.usr u
+  LEFT JOIN production.geneset g
+    ON g.usr_id = u.usr_id
+  LEFT JOIN production.project p
+    ON p.usr_id = u.usr_id
+  LEFT JOIN production.result r
+    ON r.usr_id = u.usr_id
+  LEFT JOIN extsrc_20150605.usr2gene x
+    ON x.usr_id = u.usr_id
+  LEFT JOIN production.usr2grp s
+    ON s.usr_id = u.usr_id
+  WHERE g.usr_id is NULL
+    AND p.usr_id is NULL
+    AND r.usr_id is NULL
+    AND x.usr_id is NULL
+    AND s.usr_id is NULL);
