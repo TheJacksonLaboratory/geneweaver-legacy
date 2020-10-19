cef54fca06acf5ddb71419501376b562ce648acb
emilywang2020 <60168970+emilywang2020@users.noreply.github.com>
2020-10-19 08:41:51 -0500

    created initial insert_variant_file function

--- src/variant_batch.py
+++ src/variant_batch.py
@@ -58,6 +58,7 @@ class BatchReader(batch.BatchReader):
         if not usr_id:
             usr_id = self.usr_id
 
+        print("bs " + bs)
         self.__parse_batch_syntax(bs)
 
         if self.errors:
