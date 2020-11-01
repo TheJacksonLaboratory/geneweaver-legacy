3a87bc2ef5cc9aae07d7e82927ea0b54fe5b61f5
Sam  Shenoi <samshenoi@Sams-MBP.attlocal.net>
2020-11-01 17:45:26 -0600

    Updates

--- src/genesetblueprint.py
+++ src/genesetblueprint.py
@@ -129,10 +129,10 @@ def create_batch_geneset():
     if isVariant > 0:
         print("CALLING variant_batch.BatchReader(batch_file, user_id successfully)")
         batch_reader =  variant_batch.BatchReader(batch_file, user_id)
-        return flask.jsonify({"isVarant"})
+        return flask.jsonify({isVariant: "isVarant"})
     else:
         batch_reader = batch.BatchReader(batch_file, user_id)
-        return flask.jsonify({"Not Variant"})
+        return flask.jsonify({isVariant: "Not Variant"})
 
     ## Required later on when inserting OmicsSoft specific metadata
     is_omicssoft = False
